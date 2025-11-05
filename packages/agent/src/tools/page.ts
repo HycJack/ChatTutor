883aaf35983d7d0fabe23b732300894f8bc9f71f
Acbox <acbox0328@gmail.com>
2025-11-05 23:08:10 +0800

    feat(agent): system prompt

--- packages/agent/src/tools/page.ts
+++ packages/agent/src/tools/page.ts
@@ -1,37 +1,47 @@
-import type { Page } from '@chat-tutor/shared'
+import { PageType, type Page } from '@chat-tutor/shared'
 import type { Tool } from 'xsai'
 import { tool } from 'xsai'
 import { type } from 'arktype'
+import type { CanvasPage } from '@chat-tutor/canvas'
 
 export const getPageTools = async (pages: Page[]) => {
-  const create = tool({
-    name: 'create_page',
-    description: 'Create a new page',
+  const checkExist = (id: string) => {
+    if (pages.find(page => page.id === id)) {
+      return {
+        success: false,
+        message: 'Page already exists',
+      }
+    }
+  }
+  const createCanvas = tool({
+    name: 'create_canvas',
+    description: 'Create a new canvas page',
     parameters: type({
       id: 'string',
       title: 'string',
-      type: type.enumerated('canvas')
+      range: type.number.array().describe('Range (y axis) of the coordinate system, a number tuple like [min, max]'),
+      domain: type.number.array().describe('Domain (x axis) of the coordinate system, a number tuple like [min, max]'),
     }),
-    execute: async ({ id, title, type }) => {
-      if (pages.find(page => page.id === id)) {
-        return {
-          success: false,
-          message: 'Page already exists',
-        }
+    execute: async ({ id, title, range, domain }) => {
+      const result = checkExist(id)
+      if (result) {
+        return result
       }
       pages.push({
         id,
         title,
-        type,
+        type: PageType.CANVAS,
         steps: [],
-      })
+        range: range as [number, number],
+        domain: domain as [number, number],
+      } as CanvasPage)
       return {
         success: true,
         message: 'Page created successfully',
-        id,
+        page: id,
       }
     }
   })
 
-  return await Promise.all([create]) as Tool[]
+  return await Promise.all([createCanvas]) as Tool[]
 }
--- packages/canvas/src/page.ts
+++ packages/canvas/src/page.ts
@@ -4,5 +4,10 @@ import type { ElementActionOptions } from './element'
 export type CanvasPageAction = 
   Action<typeof ElementActionOptions.infer>
 
-export type CanvasPage = Page<CanvasPageAction>
-export type Canvas3DPage = Page<CanvasPageAction>
+export type CanvasPageExtends = {
+  range: [number, number] // y axis range
+  domain: [number, number] // x axis range
+}
+
+export type CanvasPage = Page<CanvasPageAction> & CanvasPageExtends
+export type Canvas3DPage = Page<CanvasPageAction> & CanvasPageExtends
